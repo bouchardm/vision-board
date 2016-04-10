e27348d8cd8b74aa6bee415ed8d7fa3b56ad02da
Marc-Antoine Bouchard Marceau <marc-antoine.bm@libeo.com>
2016-04-10 13:13:17 -0400

    Add history + new page to add vision

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,4 +15,12 @@ class DefaultController extends Controller
     {
         return $this->render('default/index.html.twig');
     }
+
+    /**
+     * @Route("/gestion", name="gestion")
+     */
+    public function gestionAction()
+    {
+        return $this->render('default/gestion.html.twig');
+    }
 }
